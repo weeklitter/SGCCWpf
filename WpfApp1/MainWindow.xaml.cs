8a54820a9b020c0f5c3fd3fff997638fe1c3b55b
weeklitter <zhoufu758@163.com>
2019-11-06 23:15:29 +0800

    qqq

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -79,7 +79,7 @@ namespace WpfApp1
             var ofd = new OpenFileDialog();
             ofd.ShowDialog();
             planWork.Text = ofd.FileName;
-            ofd.RestoreDirectory = true;            
+            ofd.RestoreDirectory = true;
         }
 
         private void anquan_Click(object sender, RoutedEventArgs e)
@@ -92,7 +92,43 @@ namespace WpfApp1
 
         private void lihuiSave_Click(object sender, RoutedEventArgs e)
         {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "选择生成EXCEL文件夹";
+                dialog.ShowNewFolderButton = false;
+                dialog.RootFolder = Environment.SpecialFolder.Desktop;
+                dialog.ShowDialog();
+                if (dialog.SelectedPath == string.Empty)
+                {
+                    lihuiSave.Text = "请选择目录！";
+                    MessageBox.Show("请选择需要存储的位置", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+                lihuiSave.Text = dialog.SelectedPath;
+            }
 
+            if (string.IsNullOrEmpty(safeMeetDate.Text))
+            {
+                MessageBox.Show("请选择日期", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var date = safeMeetDate.Text.Replace('/', '-');
+            var safeMeetPath = AppDomain.CurrentDomain.BaseDirectory + @"\SafeMeeting\";
+            var filesList = Directory.GetFiles(safeMeetPath);
+            if (filesList.Length > 0)
+            {
+                foreach (var file in filesList)
+                {
+                    if (file.Contains(date))
+                    {
+                        File.Copy(file, lihuiSave.Text + "\\" + Path.GetFileName(file), true);
+                    }
+                }
+            }
+
+            MessageBox.Show("生成完毕", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+            return;
         }
 
         private void monthPath_Click(object sender, RoutedEventArgs e)
@@ -105,7 +141,43 @@ namespace WpfApp1
 
         private void saveCount_Click(object sender, RoutedEventArgs e)
         {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "选择生成EXCEL文件夹";
+                dialog.ShowNewFolderButton = false;
+                dialog.RootFolder = Environment.SpecialFolder.Desktop;
+                dialog.ShowDialog();
+                if (dialog.SelectedPath == string.Empty)
+                {
+                    saveCountPath.Text = "请选择目录！";
+                    MessageBox.Show("请选择需要存储的位置", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+                saveCountPath.Text = dialog.SelectedPath;
+            }
 
+            if (string.IsNullOrEmpty(countDate.Text))
+            {
+                MessageBox.Show("请选择日期", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var date = countDate.Text.Replace('/', '-');
+            var countPath = AppDomain.CurrentDomain.BaseDirectory + @"\DianliangCount\";
+            var filesList = Directory.GetFiles(countPath);
+            if (filesList.Length > 0)
+            {
+                foreach (var file in filesList)
+                {
+                    if (file.Contains(date))
+                    {
+                        File.Copy(file, saveCountPath.Text + "\\" + Path.GetFileName(file), true);
+                    }
+                }
+            }
+
+            MessageBox.Show("生成完毕", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+            return;
         }
 
         private void save_Click(object sender, EventArgs e)
@@ -125,6 +197,33 @@ namespace WpfApp1
                 savePath.Text = dialog.SelectedPath;
             }
 
+            if (string.IsNullOrEmpty(dianliangDate.Text))
+            {
+                MessageBox.Show("请选择日期", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            var date = dianliangDate.Text.Replace('/', '-');
+            var dianLiangPath = AppDomain.CurrentDomain.BaseDirectory + @"\DianLiang\";
+            var filesList = Directory.GetFiles(dianLiangPath);
+            if(filesList.Length > 0)
+            {
+                foreach(var file in filesList)
+                {
+                    if(file.Contains(date))
+                    {
+                        File.Copy(file, savePath.Text + "\\" + Path.GetFileName(file), true);
+                    }
+                }
+            }
+
+            MessageBox.Show("生成完毕", "提示", MessageBoxButton.OK, MessageBoxImage.Information);
+            return;
+
+          
+
+
+            
             //if (string.IsNullOrWhiteSpace(guangfuZuigao.Text))
             //{
             //    MessageBox.Show("请输入光伏最高负荷", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
@@ -166,103 +265,103 @@ namespace WpfApp1
             //    return;
             //}
 
-            if (string.IsNullOrWhiteSpace(e5000Path.Text))
-            {
-                MessageBox.Show("请选择E5000文件", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
+            //if (string.IsNullOrWhiteSpace(e5000Path.Text))
+            //{
+            //    MessageBox.Show("请选择E5000文件", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //    return;
+            //}
 
-            if (string.IsNullOrWhiteSpace(e3000Path.Text))
-            {
-                MessageBox.Show("请选择E3000文件", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
+            //if (string.IsNullOrWhiteSpace(e3000Path.Text))
+            //{
+            //    MessageBox.Show("请选择E3000文件", "警告信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //    return;
+            //}
 
-            var excelop = new ExcelOpService();
-            //E5000表格读取
-            var e5000ExtName = Path.GetExtension(e5000Path.Text);//判断EXCEL文件格式
-            ISheet e5000Sheet;
-            if (string.IsNullOrEmpty(e5000ExtName) || (e5000ExtName.ToUpper() != ".XLS" && e5000ExtName.ToUpper() != ".XLSX"))
-            {
-                MessageBox.Show("输入的文件格式不正确", "错误信息", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
-            else if (e5000ExtName.ToUpper() == ".XLS")
-            {
-                using (var file = new FileStream(e5000Path.Text, FileMode.Open, FileAccess.Read))
-                {
-                    var e5000Book = new HSSFWorkbook(file);
-                    e5000Sheet = e5000Book.GetSheetAt(0);
-                    file.Close();
-                }
-            }
-            else
-            {
-                using (var file = new FileStream(e5000Path.Text, FileMode.Open, FileAccess.Read))
-                {
+            //var excelop = new ExcelOpService();
+            ////E5000表格读取
+            //var e5000ExtName = Path.GetExtension(e5000Path.Text);//判断EXCEL文件格式
+            //ISheet e5000Sheet;
+            //if (string.IsNullOrEmpty(e5000ExtName) || (e5000ExtName.ToUpper() != ".XLS" && e5000ExtName.ToUpper() != ".XLSX"))
+            //{
+            //    MessageBox.Show("输入的文件格式不正确", "错误信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //    return;
+            //}
+            //else if (e5000ExtName.ToUpper() == ".XLS")
+            //{
+            //    using (var file = new FileStream(e5000Path.Text, FileMode.Open, FileAccess.Read))
+            //    {
+            //        var e5000Book = new HSSFWorkbook(file);
+            //        e5000Sheet = e5000Book.GetSheetAt(0);
+            //        file.Close();
+            //    }
+            //}
+            //else
+            //{
+            //    using (var file = new FileStream(e5000Path.Text, FileMode.Open, FileAccess.Read))
+            //    {
 
-                    var e5000Book = new XSSFWorkbook(file);
-                    e5000Sheet = e5000Book.GetSheetAt(0);
-                    file.Close();
-                }
-            }
-            // var e5000Model = excelop.GetE5000Model(e5000Sheet, guanfuInput.Text);//将E5000的数据转换为对应的dto  
-            //textBox2.Text += "读取E5000数据";
-            //E3000表格读取
-            var e3000ExtName = System.IO.Path.GetExtension(e3000Path.Text);//判断EXCEL文件格式
-            ISheet e3000Sheet;
+            //        var e5000Book = new XSSFWorkbook(file);
+            //        e5000Sheet = e5000Book.GetSheetAt(0);
+            //        file.Close();
+            //    }
+            //}
+            //// var e5000Model = excelop.GetE5000Model(e5000Sheet, guanfuInput.Text);//将E5000的数据转换为对应的dto  
+            ////textBox2.Text += "读取E5000数据";
+            ////E3000表格读取
+            //var e3000ExtName = System.IO.Path.GetExtension(e3000Path.Text);//判断EXCEL文件格式
+            //ISheet e3000Sheet;
 
-            if (string.IsNullOrEmpty(e3000ExtName) || (e3000ExtName.ToUpper() != ".XLS" && e3000ExtName.ToUpper() != ".XLSX"))
-            {
-                MessageBox.Show("输入的文件格式不正确", "错误信息", MessageBoxButton.OK, MessageBoxImage.Warning);
-                return;
-            }
-            else if (e3000ExtName.ToUpper() == ".XLS")
-            {
-                using (var file = new FileStream(e3000Path.Text, FileMode.Open, FileAccess.Read))
-                {
+            //if (string.IsNullOrEmpty(e3000ExtName) || (e3000ExtName.ToUpper() != ".XLS" && e3000ExtName.ToUpper() != ".XLSX"))
+            //{
+            //    MessageBox.Show("输入的文件格式不正确", "错误信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //    return;
+            //}
+            //else if (e3000ExtName.ToUpper() == ".XLS")
+            //{
+            //    using (var file = new FileStream(e3000Path.Text, FileMode.Open, FileAccess.Read))
+            //    {
 
-                    var e3000Book = new HSSFWorkbook(file);
-                    e3000Sheet = e3000Book.GetSheetAt(0);
-                    file.Close();
-                }
-            }
-            else
-            {
-                using (var file = new FileStream(e3000Path.Text, FileMode.Open, FileAccess.Read))
-                {
+            //        var e3000Book = new HSSFWorkbook(file);
+            //        e3000Sheet = e3000Book.GetSheetAt(0);
+            //        file.Close();
+            //    }
+            //}
+            //else
+            //{
+            //    using (var file = new FileStream(e3000Path.Text, FileMode.Open, FileAccess.Read))
+            //    {
 
-                    var e3000Book = new XSSFWorkbook(file);
-                    e3000Sheet = e3000Book.GetSheetAt(0);
-                    file.Close();
-                }
-            }
-            var e3000Model = excelop.GetE3000Model(e3000Sheet);//将E3000的数据转换为对应的dto  
-            //textBox2.Text += "读取E3000数据";
-            //模板读取
-            var modelPath = AppDomain.CurrentDomain.BaseDirectory + @"\ExcelModel\DailyElectricityModel.xlsx";
-            XSSFWorkbook endBook = null;
-            using (var file = new FileStream(modelPath, FileMode.Open, FileAccess.Read))
-            {
-                endBook = new XSSFWorkbook(file);
-                file.Close();
-            }
-            var endSheet = endBook.GetSheetAt(0);
-            //textBox2.Text += "读取模板";
+            //        var e3000Book = new XSSFWorkbook(file);
+            //        e3000Sheet = e3000Book.GetSheetAt(0);
+            //        file.Close();
+            //    }
+            //}
+            //var e3000Model = excelop.GetE3000Model(e3000Sheet);//将E3000的数据转换为对应的dto  
+            ////textBox2.Text += "读取E3000数据";
+            ////模板读取
+            //var modelPath = AppDomain.CurrentDomain.BaseDirectory + @"\ExcelModel\DailyElectricityModel.xlsx";
+            //XSSFWorkbook endBook = null;
+            //using (var file = new FileStream(modelPath, FileMode.Open, FileAccess.Read))
+            //{
+            //    endBook = new XSSFWorkbook(file);
+            //    file.Close();
+            //}
+            //var endSheet = endBook.GetSheetAt(0);
+            ////textBox2.Text += "读取模板";
 
-            //endSheet = excelop.SetE5000Value(e5000Model, endSheet);//将E5000的数据赋值到模板中
+            ////endSheet = excelop.SetE5000Value(e5000Model, endSheet);//将E5000的数据赋值到模板中
 
-            //endSheet = excelop.SetE3000Value(e3000Model, endSheet, guangfuZuigao.Text, guangfuzuigaotime.Text,
-            //quanwangzuigaofuhe.Text, quanwangzuigaotime.Text, jiaxingzuigaofuhe.Text, jiaxingzuigaotime.Text);//将E3000的数据赋值到模板中
-            //textBox2.Text += "对模板进行赋值";
-            var targetPath = savePath.Text + "\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".xlsx";
-            using (FileStream fileStream = File.Create(targetPath))
-            {
-                endBook.Write(fileStream);
-                fileStream.Close();
-            }
-            //textBox2.Text += "文件已生成";
-            MessageBox.Show("文件已生成，请到对应目录下寻找", "提示信息", MessageBoxButton.OK, MessageBoxImage.Warning);
+            ////endSheet = excelop.SetE3000Value(e3000Model, endSheet, guangfuZuigao.Text, guangfuzuigaotime.Text,
+            ////quanwangzuigaofuhe.Text, quanwangzuigaotime.Text, jiaxingzuigaofuhe.Text, jiaxingzuigaotime.Text);//将E3000的数据赋值到模板中
+            ////textBox2.Text += "对模板进行赋值";
+            //var targetPath = savePath.Text + "\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".xlsx";
+            //using (FileStream fileStream = File.Create(targetPath))
+            //{
+            //    endBook.Write(fileStream);
+            //    fileStream.Close();
+            //}
+            ////textBox2.Text += "文件已生成";
+            //MessageBox.Show("文件已生成，请到对应目录下寻找", "提示信息", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
     }
 }
